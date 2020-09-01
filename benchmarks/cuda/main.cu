cce88b88b6d2d4d17bb45021a76c0a0093f1e4f3
Zaid Qureshi <zaidhorse@gmail.com>
2020-09-01 12:06:58 -0400

    new test

--- benchmarks/cuda/main.cu
+++ benchmarks/cuda/main.cu
@@ -59,12 +59,13 @@ __device__ void read_data(page_cache_t* pc, QueuePair* qp, const uint64_t starti
 }
 __global__
 void access_kernel(QueuePair* qp, page_cache_t* pc,  uint32_t req_size, uint32_t n_reqs, unsigned long long* req_count) {
+    printf("in threads\n");
     uint64_t tid = blockIdx.x * blockDim.x + threadIdx.x;
 
     unsigned long long v = atomicAdd(req_count, 1);
 
     if (v < n_reqs) {
-        printf("in threads\n");
+
        read_data(pc, qp, v*512, 512, v);
     }
 }
